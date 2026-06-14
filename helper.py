6b34f9cd6575c4947ddbdf3706deaac5cd05cc92
Yash Raj <yashraj.yr1981.ps@gmail.com>
2026-06-14 19:13:28 +0530

    Deployable 1000 movie version

--- helper.py
+++ helper.py
@@ -1,42 +1,8 @@
-import os
-import gdown
 from joblib import load
 
-os.makedirs("model", exist_ok=True)
+movies = load("model/movies_small.joblib")
+similarity = load("model/similarity_small.joblib")
 
-MOVIES_URL = "https://drive.google.com/uc?id=115kMWMDIhHJ1lKdVbgaCyvICYQblVIhi"
-SIMILARITY_URL = "https://drive.google.com/uc?id=1fPgBdhl_Uxiek9lGVEh5xIU6sVaZKapj"
-
-if not os.path.exists("model/movies.joblib"):
-    print("Downloading movies.joblib...")
-    gdown.download(
-        url=MOVIES_URL,
-        output="model/movies.joblib",
-        quiet=False
-    )
-
-if not os.path.exists("model/similarity_final.joblib"):
-    print("Downloading similarity_final.joblib...")
-    gdown.download(
-        url=SIMILARITY_URL,
-        output="model/similarity_final.joblib",
-        quiet=False
-    )
-
-print("Loading movies...")
-movies = load("model/movies.joblib")
-print("Movies loaded successfully!")
-
-print("Loading similarity...")
-similarity = load("model/similarity_final.joblib")
-print("Similarity loaded successfully!")
-
-print("Movies Shape:", movies.shape)
-
-try:
-    print("Similarity Shape:", similarity.shape)
-except Exception as e:
-    print("Similarity Error:", e)
 
 def recommend(movie):
 
