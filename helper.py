cb0127c6a23392b53c37a28f99c2c593f52016eb
Yash Raj <yashraj.yr1981.ps@gmail.com>
2026-06-14 13:50:46 +0530

    Added Google Drive model download

--- helper.py
+++ helper.py
@@ -1,4 +1,22 @@
+import os
+import gdown
 from joblib import load
+
+# Download movies.joblib if not present
+if not os.path.exists("model/movies.joblib"):
+    gdown.download(
+        "https://drive.google.com/uc?id=115kMWMDIhHJ1lKdVbgaCyvICYQblVIhi",
+        "model/movies.joblib",
+        quiet=False
+    )
+
+# Download similarity_final.joblib if not present
+if not os.path.exists("model/similarity_final.joblib"):
+    gdown.download(
+        "https://drive.google.com/uc?id=1fPgBdhl_Uxiek9lGVEh5xIU6sVaZKapj",
+        "model/similarity_final.joblib",
+        quiet=False
+    )
 
 movies = load("model/movies.joblib")
 similarity = load("model/similarity_final.joblib")
