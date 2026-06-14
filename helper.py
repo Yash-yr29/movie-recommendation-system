f37a52aae8feba01ac7f41aec0defa0df45f86dd
Yash Raj <yashraj.yr1981.ps@gmail.com>
2026-06-14 14:36:09 +0530

    Fix gdown download

--- helper.py
+++ helper.py
@@ -2,30 +2,27 @@ import os
 import gdown
 from joblib import load
 
-# Create model folder
 os.makedirs("model", exist_ok=True)
 
-# Download movies.joblib
+MOVIES_URL = "https://drive.google.com/uc?id=115kMWMDIhHJ1lKdVbgaCyvICYQblVIhi"
+SIMILARITY_URL = "https://drive.google.com/uc?id=1fPgBdhl_Uxiek9lGVEh5xIU6sVaZKapj"
+
 if not os.path.exists("model/movies.joblib"):
     gdown.download(
-        "https://drive.google.com/file/d/115kMWMDIhHJ1lKdVbgaCyvICYQblVIhi/view?usp=sharing",
-        "model/movies.joblib",
-        fuzzy=True,
+        url=MOVIES_URL,
+        output="model/movies.joblib",
         quiet=False
     )
 
-# Download similarity_final.joblib
 if not os.path.exists("model/similarity_final.joblib"):
     gdown.download(
-        "https://drive.google.com/file/d/1fPgBdhl_Uxiek9lGVEh5xIU6sVaZKapj/view?usp=sharing",
-        "model/similarity_final.joblib",
-        fuzzy=True,
+        url=SIMILARITY_URL,
+        output="model/similarity_final.joblib",
         quiet=False
     )
 
 movies = load("model/movies.joblib")
 similarity = load("model/similarity_final.joblib")
-
 
 def recommend(movie):
 
