d63c5a8840d1904270d86ddf7504ca349818be32
Yash Raj <yashraj.yr1981.ps@gmail.com>
2026-06-14 14:08:42 +0530

    Fixed Google Drive model download

--- helper.py
+++ helper.py
@@ -2,19 +2,24 @@ import os
 import gdown
 from joblib import load
 
-# Download movies.joblib if not present
+# Create model folder
+os.makedirs("model", exist_ok=True)
+
+# Download movies.joblib
 if not os.path.exists("model/movies.joblib"):
     gdown.download(
-        "https://drive.google.com/uc?id=115kMWMDIhHJ1lKdVbgaCyvICYQblVIhi",
+        "https://drive.google.com/file/d/115kMWMDIhHJ1lKdVbgaCyvICYQblVIhi/view?usp=sharing",
         "model/movies.joblib",
+        fuzzy=True,
         quiet=False
     )
 
-# Download similarity_final.joblib if not present
+# Download similarity_final.joblib
 if not os.path.exists("model/similarity_final.joblib"):
     gdown.download(
-        "https://drive.google.com/uc?id=1fPgBdhl_Uxiek9lGVEh5xIU6sVaZKapj",
+        "https://drive.google.com/file/d/1fPgBdhl_Uxiek9lGVEh5xIU6sVaZKapj/view?usp=sharing",
         "model/similarity_final.joblib",
+        fuzzy=True,
         quiet=False
     )
 
