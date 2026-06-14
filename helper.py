74eef18df31085b7a6852e12745f9482c2f9a05e
Yash Raj <yashraj.yr1981.ps@gmail.com>
2026-06-14 14:54:11 +0530

    debug loading

--- helper.py
+++ helper.py
@@ -7,22 +7,31 @@ os.makedirs("model", exist_ok=True)
 MOVIES_URL = "https://drive.google.com/uc?id=115kMWMDIhHJ1lKdVbgaCyvICYQblVIhi"
 SIMILARITY_URL = "https://drive.google.com/uc?id=1fPgBdhl_Uxiek9lGVEh5xIU6sVaZKapj"
 
+# Download movies.joblib
 if not os.path.exists("model/movies.joblib"):
+    print("Downloading movies.joblib...")
     gdown.download(
         url=MOVIES_URL,
         output="model/movies.joblib",
         quiet=False
     )
 
+# Download similarity_final.joblib
 if not os.path.exists("model/similarity_final.joblib"):
+    print("Downloading similarity_final.joblib...")
     gdown.download(
         url=SIMILARITY_URL,
         output="model/similarity_final.joblib",
         quiet=False
     )
 
+print("Loading models...")
+
 movies = load("model/movies.joblib")
 similarity = load("model/similarity_final.joblib")
+
+print("Movies Shape:", movies.shape)
+print("Similarity Shape:", similarity.shape)
 
 def recommend(movie):
 
